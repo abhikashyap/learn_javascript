14ac78c4a9d7b3a023a17d8852f6c9250c5f4b5f
abhikashyap <abhikashyap.ds@gmail.com>
2024-05-12 13:58:20 +0530

    1st file

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "abhikashyap102gmail.com"
+var accountPassword ="123435"
+accountCity ="jaipur"
+accountState;
+console.log(accountId);
+accountEmail = "upadtedabhikashyap10@gmail.com"
+// accountState is undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* use let and const
+*/
